66c315b53cc22a57475146f7576ce9ebf88c69ee
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-05-28 15:50:33 +0900

    対話形式から引数を使用する形式に変更

--- 勤務表から小口作成ツール/createInvoice.ps1
+++ 勤務表から小口作成ツール/createInvoice.ps1
@@ -7,32 +7,37 @@
 #
 
 # 引数がない場合、処理を中断する
-if ([string]::IsNullorEmpty($Args[0])){
-    Write-Host "`r`n引数に勤務表ファイルを指定してください`r`n"
+if ([string]::IsNullorEmpty($Args[0])) {
+    Write-Host "`r`n====== 引数に勤務表ファイルを指定してください =====`r`n"
     exit
 }
 
-while ($true) {
-    # 対話形式で「何月の小口交通費請求書を作成するか？」を入力してもらう
-    $month = $input | Read-Host "何月の小口交通費請求書を作成しますか？(半角数字のみを入力)"    
+# 勤務表ファイルのファイル名から月を取り出す
+$Args[0] -match "_勤務表_(?<month>.*?)月" | Out-Null
+$month = $Matches.month
 
-    if ($month -match "[1-9]|1[12]"){
-        start-sleep -milliSeconds 300
 
-        # 勤務表が存在しているかチェック
-        if (! (Test-Path ($Args[0].fullName))) {
-            Write-Host "勤務表ファイルが存在しません。`r`n処理を中断します"
-            exit
-        }
+if ( $month -match "[1-9]|1[12]") {
+    start-sleep -milliSeconds 300
 
-        Write-Host "`r`n#######################################"
-        Write-Host (' ' + $month + " 月の小口交通費請求書を作成します。`r`nしばらくお待ちください。")
-        Write-Host "#######################################`r`n"
-        break
-    } else {
-        # 想定の入力ではない場合、もう一度入力をさせる
-        start-sleep -milliSeconds 300
-        Write-Host "`r`n半角数字でもう一度入力してください`r`n"
+    # 勤務表ファイルのフルパス取得
+    $kinmuhyouFullPath = Resolve-Path $Args[0]
+
+    # 勤務表が存在しているかチェック
+    if (! (Test-Path "$kinmuhyouFullPath")) {
+        Write-Host "勤務表ファイルが存在しません。`r`n処理を中断します"
+        exit
     }
+
+    Write-Host "`r`n#######################################"
+    Write-Host (' ' + $month + " 月の小口交通費請求書を作成します。`r`nしばらくお待ちください。")
+    Write-Host "#######################################`r`n"
+    break
 }
+else {
+    # 勤務表ファイルのフォーマットが違う場合は修正させる
+    start-sleep -milliSeconds 300
+    Write-Host " <社員番号>_勤務表_m月_<氏名>.xlsx の形式にファイル名を修正してください"
+}
+
 
