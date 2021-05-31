58ce5a2ca609569463d313d51b235b6eabb458d4
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-05-31 22:44:29 +0900

    フォルダ構造を変更。それにプログラムを合わせる

--- 勤務表から小口作成ツール/勤務表から小口作成.ps1
+++ 勤務表から小口作成ツール/勤務表から小口作成.ps1
@@ -25,15 +25,31 @@ function endExcel {
     exit
 }
 
-###########
+# シャープを使ったメッセージの表示をする関数
+# 引数1 : 文字色
+# 引数2以降 : メッセージ
+function displaySharpMessage {
+    # 変数の初期化
+    $maxLengths = 0
+    for($i=1;$i -lt $Args.length;$i++){
+        # メッセージの中で一番長い文字数を取得する
+        if( $maxLengths -lt $Args[$i].length){
+            $maxLengths = $Args[$i].length
+        }
+    }
+    # メッセージの表示
+    Write-Host ("`r`n" + '#' * ($maxLengths*2+6) + "`r`n") -ForegroundColor $Args[0]
+    for($i=1;$i -lt $Args.length;$i++){
+        Write-Host ('　　' + $Args[$i] + "　　`r`n") -ForegroundColor $Args[0]
+    }
+    Write-Host ('#' * ($maxLengths*2+6) + "`r`n") -ForegroundColor $Args[0]
+}
+
+# -------------------- 主処理 ----------------------------
+
+#=====================================================================
 ########################## 注意書きを表示。問題ない場合にはEnterを押させる。
 #=========================================================================
-
-# 引数が足りない場合、処理を中断する
-if ([string]::IsNullorEmpty($Args[0])) {
-    Write-Host "`r`n====== 引数1個目に小口交通費請求書ファイルを指定してください ======`r`n" -ForegroundColor Red
-    exit
-}
 
 # 現在日時を取得する
 $thisMonth = (Get-Date).Month
@@ -41,50 +57,57 @@ $today = (Get-Date).Day
 
 # 現在日時から作成するべき勤務表の月次を判定
 if ($today -le 24) {
-    $kinmuhyouMonth = $thisMonth -1
+    $month = $thisMonth -1
 } else {
-    $kinmuhyouMonth = $thisMonth
+    $month = $thisMonth
+}
+
+# 小口テンプレを取得
+$koguchi = Get-ChildItem -Recurse -File |? Name -Match "小口交通費・出張旅費精算明細書_テンプレ.xlsx"
+# 該当小口ファイルの個数確認
+if ($koguchi.Count -lt 1) {
+    Write-Host "`r`n該当する小口ファイルが存在しません`r`n`r`nダウンロードし直してください`r`n" -ForegroundColor Red
+    exit
+} elseif ($koguchi.Count -gt 1) {
+    Write-Host "`r`n該当する小口ファイルが多すぎます`r`n`r`nダウンロードし直してください`r`n" -ForegroundColor Red
+    exit
 }
 
 # 勤務表ファイルを取得
-$kinmuhyou = Get-ChildItem -Recurse -File |? name -Match "[0-9]{3}_勤務表_($kinmuhyouMonth)月_.+"
+$kinmuhyou = Get-ChildItem -Recurse -File |? Name -Match "[0-9]{3}_勤務表_($month)月_.+"
 
 # 該当勤務表ファイルの個数確認
 if ($kinmuhyou.Count -lt 1) {
     Write-Host "`r`n該当する勤務表ファイルが存在しません`r`n" -ForegroundColor Red
+    exit
 } elseif ($kinmuhyou.Count -gt 1) {
     Write-Host "`r`n該当する勤務表ファイルが多すぎます`r`n" -ForegroundColor Red
+    exit
 }
 
-# 勤務表ファイルのファイル名から月次を取り出す
-$kinmuhyou -Match "_勤務表_(?<month>.*?)月" | Out-Null
-$month = $Matches.month
-
-
-if ( $kinmuhyou.Name  -match "[0-9]{3}_勤務表_[1-9]|1[12]月_.+" ) {
+# 処理を始める前に、ファイルの存在チェックとファイル名のチェックを行う
+if ( $kinmuhyou.Name  -match "[0-9]{3}_勤務表_([1-9]|1[12])月_.+\.xlsx" ) {
     Start-Sleep -milliSeconds 300
 
     try {
     # 勤務表ファイルのフルパス取得
-    $kinmuhyouFullPath = Resolve-Path $kinmuhyou -ErrorAction Stop
+    $kinmuhyouFullPath = $kinmuhyou.FullName 
     } catch [Exception] {
         # 勤務表が存在しているかチェック
-        Write-Host "勤務表ファイルが存在しません。`r`n処理を中断します`r`n" -ForegroundColor Red
+        Write-Host ($month + "月の勤務表ファイルが存在しません。`r`nダウンロードしてください`r`n") -ForegroundColor Red
         exit
     }
 
     try {
     # 小口ファイルのフルパス取得
-    $koguchiFullPath = Resolve-Path $Args[0] -ErrorAction Stop
+    $koguchiFullPath = $koguchi.FullName
     } catch [Exception] {
         # 小口ファイルが存在しているかチェック
-        Write-Host "小口ファイルが存在しません。`r`n処理を中断します`r`n" -ForegroundColor Red
+        Write-Host "小口テンプレが存在しません。`r`nダウンロードし直してください`r`n" -ForegroundColor Red
         exit
     }
 
-    Write-Host "`r`n#######################################"
-    Write-Host (' 　' + $month + " 月の小口交通費請求書を作成します。`r`n　　しばらくお待ちください。")
-    Write-Host "#######################################`r`n"
+    displaySharpMessage "White" ([string]$month + " 月の小口交通費請求書を作成します。") "しばらくお待ちください。"
 }else {
     # 勤務表ファイルのフォーマットが違う場合は修正させる
     Write-Host " ######### <社員番号>_勤務表_m月_<氏名>.xlsx の形式にファイル名を修正してください #########`r`n" -ForegroundColor Red
@@ -116,6 +139,25 @@ $koguchiSheet = $koguchiBook.sheets(1)
 
 # ------------- 個人情報欄のコピー --------------
 
+# 小口の縦列カウンター
+$rowCounter = 11
+
+# 備考に書かれている勤務地を参考に小口に記入
+for ($row = 14; $row -le 44; $row++) {
+    # 備考欄の文字列
+    $workPlace = $kinmuhyouSheet.cells.item($row,27).text
+
+    # 在宅か休みの時以外
+    if ($workPlace -ne "" -and $workPlace -ne '在宅') {
+        # 1. 月日の記入
+        $koguchiSheet.cells.item($rowCounter,2) = $month
+        $koguchiSheet.cells.item($rowCounter,4) = $kinmuhyouSheet.cells.item($row,3).text
+        $rowCounter = $rowCounter + 3    
+    }
+}
+
+# ------------- 個人情報欄のコピー --------------
+
 # 現在の年を取得
 $thisYear = (Get-Date).Year
 # 1月に12月の小口を作ろうとしていたら年を一年戻す
@@ -124,16 +166,16 @@ if ($month -eq 1 -and (Get-Date).day -le 24) {
 }
 
 # 1. 年月日のコピー
-$koguchiSheet.cells.item(60,4) = $thisYear
-$koguchiSheet.cells.item(60,8) = $month
+$koguchiSheet.cells.item(78,4) = $thisYear
+$koguchiSheet.cells.item(78,8) = $month
 
 # 月の最終日を日付欄に設定
-$koguchiSheet.cells.item(60,11) = (Get-Date "$thisYear/$month/1").AddMonths(1).AddDays(-1).Day
+$koguchiSheet.cells.item(78,11) = (Get-Date "$thisYear/$month/1").AddMonths(1).AddDays(-1).Day
 
 # 2. 名前のコピー
-$koguchiSheet.cells.item(64,21) = $kinmuhyouSheet.cells.range("W7").text
+$koguchiSheet.cells.item(82,21) = $kinmuhyouSheet.cells.range("W7").text
 # 勤務表の名前が空白だった場合処理を中断する
-if ($koguchiSheet.cells.item(64,21).text -eq "") {
+if ($koguchiSheet.cells.item(82,21).text -eq "") {
     Write-Host ("`r`n" + $month + "月の勤務表に名前が記載されていません`r`n処理を中断します`r`n") -ForegroundColor Red
     endExcel
 }
@@ -142,9 +184,9 @@ if ($koguchiSheet.cells.item(64,21).text -eq "") {
 $affiliation = $kinmuhyouSheet.cells.range("W6").text
 # "部" を削除する
 $affiliation -match "(?<affliationName>.+?)部" | Out-Null
-$koguchiSheet.cells.item(62,6) = $Matches.affliationName
+$koguchiSheet.cells.item(80,6) = $Matches.affliationName
 # 勤務表の所属が空白だった場合処理を中断する
-if ($koguchiSheet.cells.item(62,6).text -eq "") {
+if ($koguchiSheet.cells.item(80,6).text -eq "") {
     Write-Host ("`r`n" + $month + "月の勤務表に所属が記載されていません`r`n処理を中断します`r`n") -ForegroundColor Red
     endExcel
 }
@@ -155,27 +197,28 @@ $haveNotStamp = $false
 # 勤務表の印鑑のあるセルをクリップボードにコピー
 $kinmuhyouSheet.range("AA7").copy() | Out-Null
 # 小口シートに印鑑をペースト
-$koguchiCell=$koguchiSheet.range("AD64")
+$koguchiCell=$koguchiSheet.range("AD82")
 $koguchiSheet.paste($koguchiCell)
 # ペースト先を編集
-$koguchiSheet.range("AD64").formula = ""
-$koguchiSheet.range("AD64").interior.colorindex = 0
+$koguchiSheet.range("AD82").formula = ""
+$koguchiSheet.range("AD82").interior.colorindex = 0
 # 罫線を編集するための宣言
 $LineStyle = "microsoft.office.interop.excel.xlLineStyle" -as [type]
 # 罫線をなしにする
-$koguchiSheet.range("AD64").borders.linestyle = $linestyle::xllinestylenone
+$koguchiSheet.range("AD82").borders.linestyle = $linestyle::xllinestylenone
 # 印鑑（オブジェクト）が増えてなさそうなら、メッセージを表示する
-if ($koguchiSheet.shapes.count -eq 68) {
+$numberOfObject = 79
+if ($koguchiSheet.shapes.count -eq $numberOfObject) {
     $haveNotStamp = $true
 }
 
 
 
+
 # 文字色の変更（全部黒に）
+$koguchiSheet.range("A1:BN90").font.colorindex = 1
 
 # 印鑑がないかもしれない場合注意喚起
 if ($haveNotStamp) {
-    Write-Host "`r`n#################################################################################" -ForegroundColor Blue
-    Write-Host "　　印鑑が勤務表に入っていない、または既定のセルからずれている可能性があります`r`n　　確認してください"  -ForegroundColor Blue
-    Write-Host "#################################################################################`r`n" -ForegroundColor Blue
+    displaySharpMessage "Blue" "印鑑が勤務表に入っていない、または既定のセルからずれている可能性があります" "確認してください"
 }
