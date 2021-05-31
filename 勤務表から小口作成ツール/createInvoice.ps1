0258191352983bc44582db3bebf85b4224937826
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-05-31 12:28:38 +0900

    小口の個人情報部分の反映ロジック完成

--- 勤務表から小口作成ツール/createInvoice.ps1
+++ 勤務表から小口作成ツール/createInvoice.ps1
@@ -1,12 +1,14 @@
 #
 # 勤務表から小口交通費請求書を作成するPowershell
 # 
-# 前提条件 : 当該powershellと同じフォルダに空の「<社員番号>_小口交通費・出張旅費精算明細書_m月_<氏名>.xlsx」が1つ存在すること
+# 前提条件 : 当該powershellと同じフォルダにフォーマットとハンコが記載された 小口交通費・出張旅費精算明細書Excelファイル が1つ存在すること
 #
 # 実行形式 : .\createInvoice.ps1 勤務表Excelファイル　小口Excelファイル
 #
 # 勤務表の形式 : <社員番号>_勤務表_m月_<氏名>.xlsx
 #
+
+# 注意書きを表示。問題ない場合にはEnterを押させる。
 
 # 引数が足りない場合、処理を中断する
 if ([string]::IsNullorEmpty($Args[0])) {
@@ -15,27 +17,37 @@ if ([string]::IsNullorEmpty($Args[0])) {
 }
 
 # 現在日時を取得する
-$tomonth = (Get-Date).Month
+$thisMonth = (Get-Date).Month
 $today = (Get-Date).Day
 
 # 現在日時から作成するべき勤務表の月次を判定
 if ($today -le 24) {
-    $kinmuhyouMonth = $tomonth
+    $kinmuhyouMonth = $thisMonth -1
 } else {
-    $kinmuhyouMonth = $tomonth + 1
+    $kinmuhyouMonth = $thisMonth
+}
+
+# 勤務表ファイルを取得
+$kinmuhyou = Get-ChildItem -Recurse -File |? name -Match "[0-9]{3}_勤務表_($kinmuhyouMonth)月_.+"
+
+# 該当勤務表ファイルの個数確認
+if ($kinmuhyou.Count -lt 1) {
+    Write-Host "`r`n該当する勤務表ファイルが存在しません`r`n"    
+} elseif ($kinmuhyou.Count -gt 1) {
+    Write-Host "`r`n該当する勤務表ファイルが多すぎます`r`n"
 }
 
 # 勤務表ファイルのファイル名から月次を取り出す
-$Args[0] -match "_勤務表_(?<month>.*?)月" | Out-Null
+$kinmuhyou -Match "_勤務表_(?<month>.*?)月" | Out-Null
 $month = $Matches.month
 
 
-if ( $Args[0]  -match "[0-9]{3}_勤務表_[1-9]|1[12]月_.+" ) {
-    start-sleep -milliSeconds 300
+if ( $kinmuhyou.Name  -match "[0-9]{3}_勤務表_[1-9]|1[12]月_.+" ) {
+    Start-Sleep -milliSeconds 300
 
     try {
     # 勤務表ファイルのフルパス取得
-    $kinmuhyouFullPath = Resolve-Path $Args[0] -ErrorAction Stop
+    $kinmuhyouFullPath = Resolve-Path $kinmuhyou -ErrorAction Stop
     } catch [Exception] {
         # 勤務表が存在しているかチェック
         Write-Host "勤務表ファイルが存在しません。`r`n処理を中断します`r`n"
@@ -44,7 +56,7 @@ if ( $Args[0]  -match "[0-9]{3}_勤務表_[1-9]|1[12]月_.+" ) {
 
     try {
     # 小口ファイルのフルパス取得
-    $koguchiFullPath = Resolve-Path $Args[1] -ErrorAction Stop
+    $koguchiFullPath = Resolve-Path $Args[0] -ErrorAction Stop
     } catch [Exception] {
         # 小口ファイルが存在しているかチェック
         Write-Host "小口ファイルが存在しません。`r`n処理を中断します`r`n"
@@ -68,8 +80,45 @@ try {
     $excel = New-Object -ComObject "Excel.Application" 
 }
 
+$excel.visible = $true
+
 # 勤務表ブックを開く
 $kinmuhyouBook = $excel.workbooks.open($kinmuhyouFullPath)
+$kinmuhyouSheet = $kinmuhyouBook.sheets( "$month"+'月')
 
 # 小口ブックを開く
 $koguchiBook = $excel.workbooks.open($koguchiFullPath)
+$koguchiSheet = $koguchiBook.sheets(1)
+
+# 勤務表の中身を小口にコピーする
+
+# 現在の年を取得
+$thisYear = (Get-Date).Year
+# 1月に12月の小口を作ろうとしていたら年を一年戻す
+if ($month -eq 1 -and (Get-Date).day -le 24) {
+    $thisYear = (Get-Date).AddYears(-1).Year
+}
+
+# 年月日のコピー
+$koguchiSheet.cells.item(60,4) = $thisYear
+$koguchiSheet.cells.item(60,8) = $month
+
+# 月の最終日を日付欄に設定
+$koguchiSheet.cells.item(60,11) = (Get-Date "$thisYear/$month/1" -Day 1).AddMonths(1).AddDays(-1).Day
+
+# 名前のコピー
+$koguchiSheet.cells.item(64,21) = $kinmuhyouBook.sheets($month+"月").cells.range("W7").text
+
+# 印鑑のコピー
+# 勤務表の印鑑のあるセルをクリップボードにコピー
+$kinmuhyouSheet.range("AA7").copy() | Out-Null
+
+# 小口シートに印鑑をペースト
+$koguchiCell=$koguchiSheet.range("AD64")
+$koguchiSheet.paste($koguchiCell)
+# ペースト先を編集
+$koguchiSheet.range("AD64").formula = ""
+$koguchiSheet.range("AD64").interior.colorindex = 0
+# 罫線を編集するための宣言
+$LineStyle = "microsoft.office.interop.excel.xlLineStyle" -as [type]
+$koguchiSheet.range("AD64").borders.linestyle = $linestyle::xllinestylenone
